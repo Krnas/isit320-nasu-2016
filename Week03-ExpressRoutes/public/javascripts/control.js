82c986acf8550eef254732fada0014a9eb19b878
Kristi Nasu <knasu12@gmail.com>
2016-10-08 19:32:49 -0700

    week03-expressRoutes

---
+++ Week03-ExpressRoutes/public/javascripts/control.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+    console.log('Document loaded in prog272');
+
+    $('#read').click(read);
+
+    function read() {
+        console.log('callRead called');
+    }
+});
+
+$(document).ready(function() {
+    console.log('Document loaded in prog272');
+
+    $('#read').click(read);
+    $('#readJson').click(readJson);
+
+    function read() {
+        console.log('callRead called');
+        foo();
+        $.getJSON('/read', function(result) {
+            console.log(result);
+            $('#display').html(JSON.stringify(result));
+        })
+    }
+
+    function callReadJson() {
+        console.log('readJson called');
+        $.getJSON('names.json', function(result) {
+            console.log(result);
+            $('#display').html(JSON.stringify(result));
+        })
+    }
+});
+
+function  add() {
+    var operatorA=$('#operatorA').val();
+    var operatorB=$('#operatorB').val();
+    console.log('operators:', operatorA, operatorB);
+    var requestQuery={operatorA: operatorA, operatorB: operatorB};
+
+    $.getJSON('/add', requestQuery, function (sum) {
+        console.log("Sum:", sum);
+        $('#display').html(JSON.stringify(sum));
+    });
+}
--- Week03-ExpressRoutes/routes/index.js
+++ Week03-ExpressRoutes/routes/index.js
@@ -6,4 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'ExpressRoutes-Nasu' });
 });
 
+router.get('/read', function(request, response) {
+  response.send([
+    {name: 'SarahLee'},
+    {name: 'Bob'}
+  ]);
+});
+
+router.get('/add', function (request, response) {
+  console.log('add method called');
+  console.log('The parameters are:', request.query);
+  console.log('OperatorA is:', request.query.operatorA);
+  var operatorA=parseInt(request.query.operatorA);
+  var operatorB=parseInt(request.query.operatorB);
+  response.send({sum: operatorA + operatorB });
+});
 module.exports = router;
