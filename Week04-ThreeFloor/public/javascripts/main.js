d7c4e143b929db2b7386c19fe23f6fef91edab2a
Kristi Nasu <knasu12@gmail.com>
2016-10-12 15:07:04 -0700

    week03

---
+++ Week04-ThreeFloor/public/javascripts/main.js
@@ -0,0 +1,25 @@
+requirejs.config({
+    baseUrl : '.',
+    paths : {
+        "jquery" : 'components/jquery/dist/jquery',
+        "bootstrap": 'components/bootstrap/dist/js/bootstrap',
+        "Three" : 'javascripts/three',
+        "control" : 'javascripts/control',
+        "floors": 'javascripts/floors'
+
+    },
+    shim : {
+        'Three' : {
+            exports: 'THREE'
+        }
+    }
+});
+
+requirejs([ 'jquery'], function($) {
+    'use strict';
+    requirejs(['bootstrap', 'Three', 'control' ], function(bootstrap, THREE, Control) {
+        $(document).ready(function() {
+            var control = new Control(THREE);
+        });
+    });
+});
