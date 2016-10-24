241432ff542636001c9e71d4d1af32b2b4b34f84
Kristi Nasu <knasu12@gmail.com>
2016-10-23 21:41:04 -0700

    week03-expressJquery

--- Week03-ExpressJQuery/public/javascripts/control.js
+++ Week03-ExpressJQuery/public/javascripts/control.js
@@ -1,13 +1,39 @@
 var MyObject = (function() {
 
-    // constructor
+    //constructor
     function MyObject() {
+        $('#sendString').click(showString);
+        $('#getItems').click(showItems);
+        $('#getMarie').click(showMarie);
+    }
+
+    function showString() {
+        $('#stringHolder').html('Send string was clicked');
+    }
+
+    function showItems() {
+        $("#myList").append('<li>' + 'item01' + '</li>');
+        $("#myList").append('<li>' + 'item02' + '</li>');
+        $("#myList").append('<li>' + 'item03' + '</li>');
     }
 
     MyObject.prototype.readyCalled = function() {
         $("#readyCalled").html("Ready was called and myObjected created");
     };
 
+    function showMarie(){
+        var marie = {
+            "firstName": "Marie",
+            "lastName": "Curie",
+            "city": "Paris",
+            "country": "France"
+        };
+        for (var property in marie) {
+            if (marie.hasOwnProperty(property)) {
+                $("#myList").append('<li>' + marie[property] + '</li>');
+            }
+        }
+    }
     return MyObject;
 }());
 
@@ -16,34 +42,3 @@ $(document).ready(function() {
     var myObject = new MyObject();
     myObject.readyCalled();
 });
-
-function MyObject() {
-    $('#sendString').click(showString);
-    $('#getItems').click(showItems);
-    $('#getJson').click(getJson);
-
-}
-
-function showString() {
-    $('#stringHolder').html('Send string was clicked');
-}
-
-function showItems() {
-    $("#myList").append('<li>' + 'item01' + '</li>');
-    $("#myList").append('<li>' + 'item02' + '</li>');
-    $("#myList").append('<li>' + 'item03' + '</li>');
-}
-
-function showMarie() {
-    var marie = {
-        "firstName": "Marie",
-        "lastName": "Curie",
-        "city": "Paris",
-        "country": "France"
-    };
-    for (var property in marie) {
-        if (marie.hasOwnProperty(property)) {
-            $("#myList").append('<li>' + marie[property] + '</li>');
-        }
-    }
-}
