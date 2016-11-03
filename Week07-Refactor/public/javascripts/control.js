9c17686137c1bfe314ffd6c351dacc76f798ef06
Kristi Nasu <knasu12@gmail.com>
2016-11-02 17:22:14 -0700

    week07-refactor

---
+++ Week07-Refactor/public/javascripts/control.js
@@ -0,0 +1,69 @@
+/**
+ * @name Control
+ */
+define(['nameController', 'querycontroller'], function(nameController, queryController) {
+    function control($routeProvider) {
+        'use strict';
+        $routeProvider.when('/databaseName', {
+            templateUrl: 'templates/DatabaseNames.html',
+            controller: nameController,
+            resolve: {
+                databaseName: nameController.databaseName,
+                allDbs: nameController.allDbs
+            }
+        }).when('/deleteDb', {
+            templateUrl: 'templates/QueryView.html',
+            controller: queryController,
+            resolve: {
+                result: queryController.delete
+            }
+        }).when('/createDb', {
+            templateUrl: 'templates/QueryView.html',
+            controller: queryController,
+            resolve: {
+                result: queryController.create
+            }
+        }).when('/insertNpcsBulk', {
+            templateUrl: 'templates/InsertReport.html',
+            controller: queryController,
+            resolve: {
+                result: queryController.insertNpcsBulk
+            }
+        }).when('/insertNpcsOneDoc', {
+            templateUrl: 'templates/InsertReport.html',
+            controller: queryController,
+            resolve: {
+                result: queryController.insertNpcsOneDoc
+            }
+        }).when('/insertDesignDoc', {
+            templateUrl: 'templates/QueryView.html',
+            controller: queryController,
+            resolve: {
+                result: queryController.design
+            }
+        }).when('/readOne', {
+            templateUrl: 'templates/QueryView.html',
+            controller: queryController,
+            resolve: {
+                result: queryController.readOne
+            }
+        }).when('/viewNpcsBulk', {
+            templateUrl: 'templates/QueryView.html',
+            controller: queryController,
+            resolve: {
+                result: queryController.viewBulk
+            }
+        }).when('/viewNpcsOneDoc', {
+            templateUrl: 'templates/QueryView.html',
+            controller: queryController,
+            resolve: {
+                result: queryController.viewOneDoc
+            }
+        }).otherwise({
+            redirectTo: '/'
+        });
+    };
+
+return findRoutes;
+
+});
