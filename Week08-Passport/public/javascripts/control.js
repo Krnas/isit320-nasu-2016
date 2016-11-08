ace570c406aaf1499035d41d4cc5df960942305c
Kristi Nasu <knasu12@gmail.com>
2016-11-07 16:29:45 -0800

    week08-passport

--- Week08-Passport/public/javascripts/control.js
+++ Week08-Passport/public/javascripts/control.js
@@ -1,3 +1,19 @@
+var Control = (function() {
+
+    function Control() {
+        console.log("Control constructor called");
+        $("#info").click(info);
+    }
+
+    var info = function() {
+        // WRITE AN AJAX OR GET JSON METHOD THAT CALLS THE /info ROUTE AND DISPLAYS THE RESULT
+        // THIS SHOULD INCLUDE THE USER INFORMATION SHOWN BELOW IN MY GOOGLE ACCOUNT SCREENSHOT
+    };
+
+    return Control;
+
+}());
+
 $(document).ready(function() {
-    'use strict';
+    var control = new Control();
 });
