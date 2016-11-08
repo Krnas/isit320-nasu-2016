d234a35449449e70d13e993ee98dc7e95316ba65
Kristi Nasu <knasu12@gmail.com>
2016-11-07 23:34:18 -0800

    week07-dataMaster

--- Week06-MazeDataReader/public/javascripts/Score.js
+++ Week06-MazeDataReader/public/javascripts/Score.js
@@ -1,9 +1,13 @@
- var score = npcData
-        var mainCharacter= {
-            name: "Robin",
-            value: 15
-    };
+ var score = (function() {
+     function score() {
+         $.getJSON('//read?docName=npcObjects', function (data) {
+             console.log(JSON.stringify(data.docs, null, 4));
+         });
+         var mainCharacter = {
+             name: "Robin",
+             value: 15
+         }
 
-
-
-
+         return score;
+     }
+ }
