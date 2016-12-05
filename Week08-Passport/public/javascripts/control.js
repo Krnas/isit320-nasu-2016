27e8ef99d2ac897ebbddfb9ebf66536273433e4d
Kristi Nasu <knasu12@gmail.com>
2016-12-05 15:04:38 -0800

    week08-passport

--- Week08-Passport/public/javascripts/control.js
+++ Week08-Passport/public/javascripts/control.js
@@ -1,27 +1,21 @@
-var Control = (function() {
+define(function() {
     'use strict';
 
     function Control() {
-
         console.log('Control constructor called');
         $('#info').click(info);
     }
 
     var info = function() {
-        $.getJSON('/info', function(result) {
-            $('#Username').html(JSON.stringify(result));
-            $('#name').html(JSON.stringify(result));
-            $('#ID').html(JSON.stringify(result));
+        $.ajax({
+            url: '/info'
+        }).done(function(serverInfo) {
+            $('#report').html(JSON.stringify(serverInfo));
+        }).fail(function(err) {
+            $('#debug').html(err);
         });
-        // WRITE AN AJAX OR GET JSON METHOD THAT CALLS THE /info ROUTE AND DISPLAYS THE RESULT
-        // THIS SHOULD INCLUDE THE USER INFORMATION SHOWN BELOW IN MY GOOGLE ACCOUNT SCREENSHOT
     };
 
     return Control;
 
-}());
-
-$(document).ready(function() {
-    'use strict';
-    var control = new Control();
 });
--- Week08-Passport/routes/login-facebook.js
+++ Week08-Passport/routes/login-facebook.js
@@ -1,36 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var Strategy = require('passport-facebook').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 /**************************************
  *  Facebook
  **************************************/
 
 router.get('/profile', require('connect-ensure-login').ensureLoggedIn(),
-    function(req, res) {
+    function(request, response) {
         'use strict';
-        console.log(req.user);
-        res.render('profile-facebook', {
+        console.log(request.user);
+        response.render('profile-facebook', {
             title: 'Facebook Profile',
-            user: req.user
+            user: request.user,
+            userInfo: JSON.stringify(request.user, null, 4)
         });
     });
 
-passport.use(new Strategy({
+passport.use(new FacebookStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: 'http://localhost:30025/facebook/login/callback'
+        callbackURL: 'http://localhost:30025/facebook/login/return',
+        profileFields: ['id', 'displayName', 'photos', 'email']
     },
     function(accessToken, refreshToken, profile, done) {
         'use strict';
+        console.log('accessToken', accessToken);
+        console.log('refreshToken', refreshToken);
+        console.log('profile', profile);
+        // In this example, the user's Facebook profile is supplied as the user
+        // record.  In a production-quality application, the Facebook profile should
+        // be associated with a user record in the application's database, which
+        // allows for account linking and authentication with other identity
+        // providers.
         return done(null, profile);
     }));
 
 router.get('/login',
     passport.authenticate('facebook'));
 
-router.get('/login/callback',
+//router.get('/login/facebook/return',
+router.get('/login/return',
     passport.authenticate('facebook', {
         failureRedirect: '/login'
     }),
