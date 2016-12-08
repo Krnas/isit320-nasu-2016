4d4818600e6923d461d88a1d6378f89d23a67de6
Kristi Nasu <knasu12@gmail.com>
2016-12-08 13:23:37 -0800

    datamaster final

--- Week07-Refactor/routes/CouchDesignDocs.js
+++ Week07-Refactor/routes/CouchDesignDocs.js
@@ -1,101 +1,76 @@
+/**
+ * @name CouchDesignDoc
+ */
+
+/* globals emit: true */
+
 function designDocs(router, nano, dbName) {
     'use strict';
 
-    var firstAndLast = function(doc) {
-        if (doc.firstName && doc.lastName) {
-            var name = doc.firstName + ' ' + doc.lastName;
-            emit(doc._id, name);
+    var elfSessions = function(doc) {
+        if (doc.collectionName === 'sessions') {
+            emit(doc._id, doc);
         }
     };
 
-    var lastOnly = function(doc) {
-
-        if (doc.firstName && doc.lastName) {
-            var name = doc.lastName;
-            emit(doc._id, name);
+    var npcsBulkNoOneDoc = function(doc) {
+        if (doc._id !== 'npcsDoc') {
+            emit(doc._id, doc);
         }
     };
 
-    var docIdDoc = function(doc) {
-        emit(doc._id, doc);
+    var npcsBulk = function(doc) {
+        if (doc._id !== 'npcsDoc') {
+            emit(doc._id, doc);
+        }
     };
 
-    var docBulk = function(doc) {
-        emit(doc._id, doc.name);
-    };
-
-    var docNpcsCapital = function(doc) {
-        if (doc._id=== 'npcObjects') {
+    var npcsOneDoc = function(doc) {
+        if (doc._id === 'npcsDoc') {
             var data = [];
-            doc.docs.forEach(function(npcs) {
+            doc.docs.forEach(function(npc) {
                 data.push({
-                    'npc_id': doc.npc_id,
-                    'npc_name': doc.npc_name,
-                    'value': doc.value
+                    'npc_name': npc.npc_name,
+                    'description': npc.description,
+                    'value': npc.value
                 });
             });
-            emit(doc.docs[0].abbreviation, data);
+            emit(doc._id, data);
         }
-
-    };
-
-    var docNpcsDoc = function(doc) {
-        if (doc._id === 'npcObjects') {
-            var data = [];
-            doc.docs.forEach(function(npcs) {
-                data.push({
-                    'npc_id': doc.npc_id,
-                    'npc_name': doc.npc_name,
-                    'question': doc.question,
-                    'answer': doc.answer
-                });
-            });
-            emit(doc.docs[0].abbreviation, data);
-        }
-        nanoDb.insert(npcs, docName, function(err, body) {
-            if (!err) {
-                console.log(body);
-            } else {
-                console.log(err);
-            }
-        });
     };
 
     function createDesignDocument(designDocument, designName, response) {
         var nanoDb = nano.db.use(dbName);
         nanoDb.insert(designDocument, designName, function(error, body) {
             if (!error) {
-                console.log(body);
-                response.send(body);
+                var result = {
+                    ok: true,
+                    data: body
+                };
+                console.log(result);
+                response.send(result);
             } else {
                 console.log('error: ' + error);
-                response.send({
-                    'Result': 'The document might already exist. ' + error
-                });
+                response.send(error);
             }
         });
     }
 
     router.get('/designDoc', function(request, response) {
-
         console.log('Design Doc Called');
 
-        var designName = '_design/npcs';
+        var designName = '_design/game';
         var designDocument = {
             'views': {
-                'docBulk': {
-                    'map': docBulk
+                'npcsBulk': {
+                    'map': npcsBulk
                 },
-                'docIdDoc': {
-                    'map': docIdDoc
+                'npcsOneDoc': {
+                    'map': npcsOneDoc
                 },
-                'docNpcsCapital': {
-                    'map': docNpcsCapital
-                },
-                'docNpcsDoc': {
-                    'map': docNpcsDoc
+                'elf-sessions': {
+                    'map': elfSessions
                 }
-
             }
         };
 
--- Week07-Refactor/routes/CouchViews.js
+++ Week07-Refactor/routes/CouchViews.js
@@ -1,84 +1,60 @@
 function views(router, nano, dbName) {
     'use strict';
-
-    function runTemplateView(templateName, request, response) {
-        var templater = require('../Library/Templater');
+    router.get('/viewNpcsBulk', function(request, response) {
+        console.log('/viewNpcsBulk called', request.query);
         var nanoDb = nano.db.use(dbName);
-        nanoDb.view(request.query.designDoc, request.query.view, function(err,
-            body) {
+        nanoDb.view(request.query.designDoc, request.query.view, function(err, body) {
             if (!err) {
                 console.log(body);
-
-                var result = [];
+                var result = {
+                    docs: []
+                };
                 body.rows.forEach(function(doc) {
-                    result.push(doc.value);
-                    console.log(doc.value);
+                    result.docs.push(doc.value);
                 });
-                var html = templater.template.addNames(templateName, result);
-                response.send(html);
+                response.send(result);
             } else {
                 console.log(err);
                 response.send(500, err);
             }
         });
-    }
-
-    /**
-     * @memberOf CouchViews
-     * @name View01 http://localhost:5984/couch_views/_design/states/_view/docBulk
-     */
-    router.get('/viewBulkTemplate', function(request, response) {
-        console.log('viewDocBulk Called: ' + request.query);
-        runTemplateView('Templates/Basic.html', request, response);
     });
 
-    /**
-     * @memberOf CouchViews
-     */
-    router.get('/viewStateCapitalTemplate', function(request, response) {
-        console.log('viewStateCapital Called: ' + request.query);
-        runTemplateView('Templates/StateCapital.html', request, response);
-    });
-
-    router.get('/viewBulk', function(request, response) {
+    router.get('/viewSessions', function(request, response) {
+        console.log('/viewSessions called', request.query, dbName);
         var nanoDb = nano.db.use(dbName);
         nanoDb.view(request.query.designDoc, request.query.view, function(err, body) {
             if (!err) {
                 console.log(body);
-                response.send(body);
+                //var data = body.rows[0].value.map(function(a) {
+                //    return a;
+                //});
+                response.send({
+                    'name': 'viewSessions',
+                    docs: body
+                });
             } else {
                 console.log(err);
-        response.status(err.statusCode).send(err);
+                response.status(err.statusCode).send(err);
             }
         });
     });
 
-    router.get('/viewStateCapitalAngular', function(request, response) {
-        console.log('viewStateCapitalAngular called.');
-        var doc = request.query.designDoc;
-        var view = request.query.view;
-        var nanoDb = nano.db.use(dbName);
-        nanoDb.view(doc, view, function(err, body) {
-            if (!err) {
-                console.log(body);
-                response.send(body);
-            } else {
-                console.log(err);                
-        response.status(err.statusCode).send(err);
-            }
-        });
-    });
-
-    router.get('/viewOneDoc', function(request, response) {
-		console.log('View one doc called');
+    router.get('/viewNpcsOneDoc', function(request, response) {
+        console.log('/viewNpcsOneDoc called', request.query);
         var nanoDb = nano.db.use(dbName);
         nanoDb.view(request.query.designDoc, request.query.view, function(err, body) {
             if (!err) {
-                console.log(body);
-                response.send(body);
+                var data = body.rows[0].value.map(function(a) {
+                    return a;
+                });
+                response.send({
+                    'name': 'viewNpcsOneDoc',
+                    docs: data
+                });
             } else {
                 console.log(err);
-        response.status(err.statusCode).send(err);
+                response.status(err.statusCode).send(err);
             }
         });
     });
